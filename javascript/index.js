32743290aa3e2d965fd0714b4ecb37dcee556a58
Danitza28 <danielnasir28@gmail.com>
2023-07-29 11:37:50 +0300

    First Commit

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+// var x=10;
+//var y=20;
+// if (x>=y){
+    // document.write("yes true")
+// }
+// else if(x==y){
+    // document.write("nope")
+// }
+// else{
+    // document.write("maybe")
+// }
+// This function clear all the values
+
+
